93222a5933800e50a7be403d86ade360b09eefec
Hans Allendorfer <hansallendorfer@yahoo.com>
2023-06-13 17:42:01 -0700

    add navigation component, add blank image effects, filters demo screens

--- app/src/main/java/com/example/magpie/MainActivity.kt
+++ app/src/main/java/com/example/magpie/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.magpie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.magpie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
+        setContentView(binding.root)
     }
 }
